dcb4e6714660ac6e590f85c24b7cf4577c74468c
njokujr <njokuvictory216@gmail.com>
2022-09-08 22:35:04 +0100

    C program to print texts exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+
 /**
- *main - writes out a string to the stdout
+ *main - print
  *
- *Return: string value of stng
+ *Return: Always 0 (Successful)
  */
 int main(void)
 {
-puts = "\"Programming is like building a multilingual puzzle";
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
