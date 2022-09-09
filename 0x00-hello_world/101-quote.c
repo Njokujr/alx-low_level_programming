0617f89e7c544fd7930111216cb4dc9f080825f0
njokujr <njokuvictory216@gmail.com>
2022-09-09 03:40:40 -0700

    update..

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-i#include <unistd.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,10 +6,9 @@ i#include <unistd.h>
  */
 int main(void)
 {
-     char str1[] = "and that piece of art
+char str1[] = "and that piece of art
 is useful\" - Dora Korpar,
 2015-10-19\n";
-
-     write(2, str1, 59);
-     return (1);
+write(2, str1, 59);
+return (1);
 }
