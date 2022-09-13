462183fe074b6cc226f83c9a9913b7c140140c6f
njokujr <njokuvictory216@gmail.com>
2022-09-13 10:55:08 -0700

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "_putchar.h"
+
+/**
+ * main - Entry point
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = ; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
