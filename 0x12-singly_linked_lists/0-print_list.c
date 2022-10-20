3e676b5c2ce905804cae27e0559d98d9c64289d3
njokujr <njokuvictory216@gmail.com>
2022-10-20 13:13:35 -0700

    corr..

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
-
-	for (i = 0; h; i++)
+	size_t nodes = 0;
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
 		h = h->next;
 	}
-	return (i);
+	return (nodes);
 }
