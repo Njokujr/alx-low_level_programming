1b6cd535c50d2c8d23907233306fd88bd9bcf268
njokujr <njokuvictory216@gmail.com>
2022-10-25 13:57:26 -0700

    Q_103

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that frees a listint_t list.
+ * @h: A pointer listint_t structure
+ * Return: The size of the list that was free'd
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	h++;
+	return (0);
+}
