caa1a8232392378ce8e364cfef7b609ddf5f4f6e
njokujr <njokuvictory216@gmail.com>
2022-10-25 14:23:52 -0700

    Q_102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
-
+#include <stdlib.h>
 /**
  * free_listint_safe - function that frees a listint_t list.
  * @h: A pointer listint_t structure
@@ -9,6 +9,39 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	h++;
-	return (0);
+	size_t counter = 0;
+	listint_t *temp;
+
+	temp = *h;
+	while (temp)
+	{
+		temp = *h;
+		temp = temp->next;
+		free_list(temp);
+		counter++;
+	}
+	*h = NULL;
+
+	return (counter);
+
+}
+
+/**
+ * free_list - A function that frees a listint_t recursively
+ * @head: A pointer to the listint_t structure
+ * Return: Nothing
+ */
+
+void free_list(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head)
+	{
+		temp = head;
+		temp = temp->next;
+		free(temp);
+		free_list(temp);
+	}
+	free(head);
 }
