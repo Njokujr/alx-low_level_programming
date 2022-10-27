9a3bb492577ec04df7335dea049fa70741698e6d
njokujr <njokuvictory216@gmail.com>
2022-10-27 12:24:53 -0700

    Mandatory tasks_0-5

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,22 +10,10 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max_value = 32768; /* 1000 0000 0000 0000 */
+	void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (max_value)
-	{
-		if (flag == 1 && (n & max_value) == 0)
-			_putchar('0');
-		else if ((n & max_value) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max_value >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
