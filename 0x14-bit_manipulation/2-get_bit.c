b8dfb8aac03994a9880ed91cb6ff0b9cbbe45693
njokujr <njokuvictory216@gmail.com>
2022-10-27 12:30:00 -0700

    Mandatory tasks_0-5

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,18 +7,16 @@
  * @index: The index where the bit get at
  * Return: The value of the bit at index index or -1 if an error occured.
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max_value = 0x01;
-
-	max_value <<= index;
 	if
-		(max_value == 0)
+		(index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	if
-		((n & max_value))
-		return (1);
-	else
+		((n & (1 << index)) == 0)
 		return (0);
+
+	return (1);
 }
